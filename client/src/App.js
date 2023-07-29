84fef6967fa39bc10b0c639025c9002d762f1cda
jupyter0323 <andikeller1109@gmail.com>
2023-07-29 10:56:03 -0400

    ui_com1

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Alert, Container, Form, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="w-25" fluid>
+      <Alert className="mt-5" variant={"success"} dismissible>
+        Hey, You got a new Message !!
+      </Alert>
+      <Form.Group className="mt-5" controlId="formBasicEmail">
+        <Form.Control placeholder="Enter text" />
+      </Form.Group>
+
+      <Button className="mt-2" variant="primary">
+        Broadcast
+      </Button>
+    </Container>
   );
 }
 
