a1cb5affb856e3a92684f7d4ca17a66a4f19a942
jupyter0323 <andikeller1109@gmail.com>
2023-07-29 11:54:46 -0400

    websocket_server_com2

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
-const io = require("socket.io")("server", { cors: { origin: "*" } });
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
   console.log("New User Connected!");
