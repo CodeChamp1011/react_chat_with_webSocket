b16a43aa6ef8ec94da0d7cd70366307ae30900fc
jupyter0323 <andikeller1109@gmail.com>
2023-07-29 11:11:32 -0400

    create_server_file

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const io = require("socket.io")("server", { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("New User Connected!");
+});
+
+const PORT = 3030;
+const URL = `https://localhost:${PORT}/`;
+
+server.listen(PORT, () => console.log(`listening on ${URL}`));
